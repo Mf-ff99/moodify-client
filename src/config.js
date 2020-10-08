684734944a4a02f3dca1df3941f88ff2c3a4befa
Mark Force <force.j.mark@gmail.com>
2020-10-08 11:15:09 -0400

    fixed API error, "/"

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
-    API_ENDPOINT: ' https://moodify-server-api.herokuapp.com/',
+    API_ENDPOINT: ' https://moodify-server-api.herokuapp.com',
     TOKEN_KEY: 'moodify-client-auth-token',
   }
