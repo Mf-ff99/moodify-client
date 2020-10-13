10cd9a4bd2a2aedfade2bfd983c1a3c701671a0f
Mark Force <force.j.mark@gmail.com>
2020-10-13 16:56:15 -0400

    fixed home-page link bug, animation bug

--- src/components/bg-animation/bg-animation.js
+++ src/components/bg-animation/bg-animation.js
@@ -5,12 +5,12 @@ export default class BGAnimation extends React.Component {
 
     render() {
         return (
-            <section className="animation-footer">
+            <footer className="animation-footer">
                 <div className="wave wave1"></div>
                 <div className="wave wave2"></div>
                 <div className="wave wave3"></div>
                 <div className="wave wave4"></div>
-            </section>
+            </footer>
         )
     }
 }
