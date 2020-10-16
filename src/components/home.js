5e21f519776138c92464ea4a9f8008ec5f690402
Mark Force <force.j.mark@gmail.com>
2020-10-16 09:38:50 -0400

    fixed landing page bug

--- src/components/home.js
+++ src/components/home.js
@@ -19,52 +19,6 @@ export default class Home extends React.Component {
       })
   }
 
-  handleFiltering = (e) => {
-    const filterVar = e.target.value
-    let filteredMoods
-    let allMoods = this.state.moods
-    let sortedMoods = allMoods.sort(function (x, y) {
-      return new Date(y.date_added) - new Date(x.date_added);
-    })
-    var seventhDay = new Date();
-    var thirtyDay = new Date();
-    
-    seventhDay.setDate(seventhDay.getDate() - 7)
-    thirtyDay.setDate(thirtyDay.getDate() - 30)
-    // console.log('seventh day ', seventhDay.getTime())
-
-    switch (filterVar) {
-      case 'pastWeek':
-        //call function to query the DB and set
-        //state to an array filtered by past week
-        filteredMoods = sortedMoods.filter((d) => {
-          // console.log(new Date(d.date_added).getTime() >= seventhDay.getTime())
-          if(new Date(d.date_added).getTime() >= seventhDay.getTime()) {
-            return d;
-          } else {
-            return null;
-          };
-        });
-        this.setState({
-          moods: filteredMoods,
-        })
-        break;
-      case 'pastMonth':
-        sortedMoods.filter((d) => {
-          return new Date(d.date_added).getTime() >= thirtyDay.getTime();
-        })
-        console.log('pastMonth ', allMoods)
-        this.setState({
-          moods: sortedMoods,
-        })
-        break;
-      case 'allTime':
-        return;
-      default:
-        return;
-    }
-  }
-
 
   render() {
     let allMoods = this.state.moods
@@ -75,30 +29,24 @@ export default class Home extends React.Component {
     return (
       <div className="home-view">
         <div className="quote-generator-container">
-        <QuoteGenerator />
+          <QuoteGenerator />
         </div>
         <section className="home-page">
-          
+
           <div className="mood-display" id="mood-display">
             <h1>How have you been feeling recently?</h1>
             <div className="add-mood-filter-btn-container">
-              
-              {/* <select name="filter" onChange={this.handleFiltering}>
-                <option value="pastWeek">Past week</option>
-                <option value="pastMonth">Past month</option>
-                <option value="allTime">All time</option>
-              </select> */}
             </div>
-            
-            
+
+
             <br />
             <div className="chart-container">
-            <Chart props={this.state.moods} />
+              <Chart props={this.state.moods} />
             </div>
-              
+
           </div>
           <div className="past-moods">
-              <Link to="/add-mood"><button type="click" className="add-mood-btn">Add a mood</button></Link>
+            <Link to="/add-mood"><button type="click" className="add-mood-btn">Add a mood</button></Link>
             <ul id="mood-list" className="past-moods-list">
               <br />
               {sortedMoods.map(mood => {
